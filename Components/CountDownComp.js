33311f8d1206eda74ecb4879f6376ff7dd39139f
Tharuka Nimesha <nimesha1996@gmail.com>
2019-03-24 07:57:45 +0530

    Countdown integration

---
+++ Components/CountDownComp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CountDown from 'react-native-countdown-component';
+
+const CountDownComp = props => {
+    return (
+      <CountDown
+        size={30}
+        until={1000}
+        onFinish={() => alert('Finished')}
+        digitStyle={{backgroundColor: '#FFF', borderWidth: 2, borderColor: '#1CC625'}}
+        digitTxtStyle={{color: '#1CC625'}}
+        timeLabelStyle={{color: 'red', fontWeight: 'bold'}}
+        separatorStyle={{color: '#1CC625'}}
+        timeToShow={['H', 'M', 'S']}
+        timeLabels={{m: null, s: null}}
+        showSeparator
+      />
+    );
+}
+
+export default CountDownComp;
