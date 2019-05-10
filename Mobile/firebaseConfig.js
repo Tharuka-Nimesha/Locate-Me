e30d361345ac1e6cdcd03c7fd9cb3cbef516da90
Tharuka Nimesha <nimesha1996@gmail.com>
2019-05-10 18:06:57 +0530

    Update firebaseConfig.js

--- Mobile/firebaseConfig.js
+++ Mobile/firebaseConfig.js
@@ -1,8 +1,8 @@
 export var config = {
   apiKey: "",
-  authDomain: "track-of-truck.firebaseapp.com",
-  databaseURL: "https://track-of-truck.firebaseio.com",
-  projectId: "track-of-truck",
-  storageBucket: "track-of-truck.appspot.com",
-  messagingSenderId: "642844600341"
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
